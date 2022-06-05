f57502505f120cebe07cdf37c0831369afaad25f
John Doe <johndoe@example.com>
2022-06-05 08:09:33 -0400

    initial commit

---
+++ makefile
@@ -0,0 +1,30 @@
+twine_upload: ensure-git update-version clean sdist bdist_wheel twine_check
+
+help:
+	@echo 'Target name is simply passed to setup.py . make build, make install, make bdist_wheel ...'
+
+%:
+	python3 setup.py "$@"
+
+update-version:
+	-git submodule update --init --recursive
+	pushd RandomX &&\
+        git fetch origin &&\
+        describe=$$(git describe --tags origin/master) &&\
+        tag=$${describe%%-*} &&\
+        version=$${tag#v} &&\
+        git checkout "$$tag" &&\
+        popd &&\
+        echo "$$version" | tee version
+	git add RandomX version
+
+ensure-git:
+	cd test; pytest .. --rootdir=.
+	git update-index --refresh 
+	git diff-index --quiet HEAD --
+	git status
+	u="$$(git ls-files --others --exclude-standard)" && test -z "$$u"
+	git push
+
+twine_check:
+	pip3 install keyring==21.0.0 setuptools-twine
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+import glob, os, platform
+
+machine = platform.machine()
+
+# TODO: build submodule with cmake
+compile_flags = ['-march=native','-std=c++11','-fpic', '-O3']
+
+source_root_dir = os.path.join('RandomX', 'src')
+sources = []
+sources.extend(glob.glob('*.c', root_dir=source_root_dir))
+sources.extend(glob.glob('*.cpp', root_dir=source_root_dir))
+sources.extend(glob.glob('*.S', root_dir=source_root_dir))
+sources = [source for source in sources if 'jit_' not in source]
+if machine in ['i386', 'i686', 'x86_64']:
+    sources.extend(['jit_compiler_x86.cpp', 'jit_compiler_x86_static.S'])
+elif machine in ['aarch64_be', 'aarch64', 'armv8b', 'armv8l']:
+    sources.extend(['jit_compiler_a64_static.s', 'jit_compiler_a64.cpp'])
+    compile_flags.append('-DHAVE_HWCAP')
+sources = [os.path.join(source_root_dir,source) for source in sources]
+
+setup(
+    name='RandomX',
+    version=open('version').read(),
+    ext_modules=cythonize(
+        Extension(
+            'randomx', [
+                'randomx.pyx',
+                *[source for source in sources if source.endswith('.cpp') or source.endswith('.c')]
+            ],
+            extra_objects=[source for source in sources if source.endswith('.S')],
+            extra_compile_args=compile_flags,
+            #libraries=['randomx'],
+            language='c++',
+        ),
+        compiler_directives=dict(
+            language_level='3',
+            embedsignature=True
+        )
+    )
+)
